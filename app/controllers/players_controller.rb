a6205cde4b1166c05051344a7bd4adbd4bbafabf
PamBWillenz <pam@positek.net>
2025-10-13 17:01:17 -0400

    Make betting, deal cards turbo frames, add frames to players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,14 @@ class PlayersController < ApplicationController
     @player.update(bet: amount)
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@player, :bet), partial: "players/player_bet", locals: { player: @player }) }
+      dom = helpers.dom_id(@player, :bet)
+      # Replace the player's bet frame
+      streams = []
+      streams << turbo_stream.replace(dom, partial: "players/player_bet", locals: { player: @player })
+      # Also replace the deal button frame so its disabled state updates immediately
+      deal_dom = helpers.dom_id(@player.game, :deal_button)
+      streams << turbo_stream.replace(deal_dom, partial: "games/deal_button", locals: { game: @player.game })
+      format.turbo_stream { render turbo_stream: streams }
       format.html { redirect_back fallback_location: game_path(@player.game) }
     end
   end
